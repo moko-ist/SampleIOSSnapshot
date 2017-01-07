00b2f27a8a8aee20f93bcf91d517559dd8604395
moko <yukie.hr@gmail.com>
2017-01-08 00:16:19 +0900

    Add steps to display all the views on ui tests

--- SampleIOSSnapshotUITests/SampleIOSSnapshotUITests.swift
+++ SampleIOSSnapshotUITests/SampleIOSSnapshotUITests.swift
@@ -31,6 +31,15 @@ class SampleIOSSnapshotUITests: XCTestCase {
     func testExample() {
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        
+        let app = XCUIApplication()
+        let tablesQuery = app.tables
+        tablesQuery.staticTexts["Go to the First View"].tap()
+        app.buttons["Button"].tap()
+        app.navigationBars["First View"].buttons["Home"].tap()
+        tablesQuery.staticTexts["Go to the Second View"].tap()
+        app.navigationBars["Second View"].buttons["Home"].tap()
+        
     }
     
 }
